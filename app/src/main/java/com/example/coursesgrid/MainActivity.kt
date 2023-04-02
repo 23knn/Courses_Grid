c38e587a0a24c92f6a0ea81613e2b394d6211dca
nguch <kelvin.1.ngure@gmail.com>
2023-04-02 23:02:44 +0300

    add images, topic names and course count to course capsules in the list

--- app/src/main/java/com/example/coursesgrid/MainActivity.kt
+++ app/src/main/java/com/example/coursesgrid/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.coursesgrid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +13,8 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.example.coursesgrid.data.DataSource
 import com.example.coursesgrid.models.Topic
 
@@ -19,32 +22,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CoursesApp(courses = DataSource.topics, modifier = Modifier.fillMaxWidth())
+            CoursesApp(topics = DataSource.topics, modifier = Modifier.fillMaxWidth())
             // because data source is a singleton, you cant instantiate. just call it
         }
     }
 }
 
 @Composable 
-fun CoursesApp(courses: List<Topic>, modifier: Modifier = Modifier){
-    Surface {
-        LazyColumn {
-            items(courses){course ->
-                CourseCapsule()
-
-
+fun CoursesApp(topics: List<Topic>, modifier: Modifier = Modifier){
+    var count = 1
+    Surface(modifier = modifier){
+        LazyColumn(modifier = modifier){
+            items(topics){topic ->
+                CourseCapsule(topic, modifier = modifier)
+                println(count++)
             }
         }
     }
 }
 
 @Composable
-fun CourseCapsule(){
-    Row{
-        Text("Image")
-        Column{
-            Text("Name")
-            Text("Count")
+fun CourseCapsule(topic: Topic, modifier: Modifier = Modifier){
+    Row(modifier = modifier){
+        Image(
+            painter = painterResource(id = topic.TopicIcon),
+            contentDescription = "stringResource(id = topic.TopicName)"
+        )
+        Column(modifier = modifier){
+            Text(text = stringResource(id = topic.TopicName))
+            Text(text = topic.NumberOfCourses.toString())
         }
     }
 }
